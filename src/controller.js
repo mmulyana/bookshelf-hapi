d31a829cdee61fd234ff7f8c3e9ea0db18e5a4ef
mmulyana <mulyan.t20@gmail.com>
2024-05-07 09:31:38 +0700

    feat: add controller

---
+++ src/controller.js
@@ -0,0 +1,40 @@
+const { nanoid } = require('nanoid')
+const { createBookSchema } = require('./utils')
+const bookServices = require('./service')
+
+function createBookHandler(req, h) {
+  try {
+    const validated = createBookSchema(req.payload)
+    if (!validated.ok) {
+      throw new Error(validated.message)
+    }
+
+    const payload = validated.data
+    payload.id = nanoid(16)
+    payload.reading ??= false
+    payload.finished = payload.readCount === payload.readPage
+    payload.insertedAt = new Date().toISOString()
+    payload.updatedAt = new Date().toISOString()
+
+    bookServices.create(payload)
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: payload.id,
+      },
+    })
+    response.code(201)
+    return response
+  } catch (error) {
+    const response = h.response({
+      status: 'fail',
+      message: error.message,
+    })
+    response.code(400)
+    return response
+  }
+}
+
+module.exports = { createBookHandler }
